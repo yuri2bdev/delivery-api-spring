b4b476a66620ca11b78a9a7063b87cd0b8607028
yuri2bdev <yuribritodev@gmail.com>
2023-06-20 01:52:30 -0300

    Update PedidoController.java

--- src/main/java/br/com/delivery/deliveryapi/controller/PedidoController.java
+++ src/main/java/br/com/delivery/deliveryapi/controller/PedidoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/pedidos")
+@RequestMapping("/api/v1/pedidos")
 @Tag(name = "Pedidos", description = "API para gerenciamento de pedidos")
 public class PedidoController {
 
