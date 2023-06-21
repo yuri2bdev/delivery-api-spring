9ae62d3ffd93451acb0322a22854d6bb87445d83
yuri2bdev <yuribritodev@gmail.com>
2023-06-21 19:03:30 -0300

    Update UsuarioService.java

--- src/main/java/br/com/delivery/deliveryapi/service/UsuarioService.java
+++ src/main/java/br/com/delivery/deliveryapi/service/UsuarioService.java
@@ -9,12 +9,18 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class UsuarioService implements UserDetailsService {
 
     private final UsuarioRepository usuarioRepository;
     private final PasswordEncoder passwordEncoder;
+
+    public List<Usuario> listarUsuarios() {
+        return usuarioRepository.findAll();
+    }
 
     public Usuario cadastrarUsuario(Usuario usuario) {
         usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
