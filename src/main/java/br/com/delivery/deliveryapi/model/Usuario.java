015a9fe52ba4383402ccab5f40e5dec28185eb18
yuri2bdev <yuribritodev@gmail.com>
2023-06-22 00:33:31 -0300

    Update Usuario.java

--- src/main/java/br/com/delivery/deliveryapi/model/Usuario.java
+++ src/main/java/br/com/delivery/deliveryapi/model/Usuario.java
@@ -3,8 +3,10 @@ package br.com.delivery.deliveryapi.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -13,18 +15,16 @@ import javax.persistence.*;
 public class Usuario {
 
     @Id
-    @Column (name = "usuario_id")
-    private Long id;
-
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    @Column(name = "usuario_id")
+    private Long idUsuario;
     @Column(name = "username")
     private String username;
-
     @Column(name = "password")
     private String password;
-
     @Column(name = "email")
     private String email;
-
     @Column(name = "telefone")
     private String telefone;
 }
