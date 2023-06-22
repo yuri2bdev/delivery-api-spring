8797bd6d9faac38dbb475b79add53a66eda4e77f
yuri2bdev <yuribritodev@gmail.com>
2023-06-22 00:33:29 -0300

    Update Cliente.java

--- src/main/java/br/com/delivery/deliveryapi/model/Cliente.java
+++ src/main/java/br/com/delivery/deliveryapi/model/Cliente.java
@@ -1,8 +1,10 @@
 package br.com.delivery.deliveryapi.model;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
@@ -14,8 +16,10 @@ import javax.persistence.*;
 public class Cliente {
 
     @Id
-    @Column (name = "cliente_id")
-    private Long id;
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    @Column(name = "cliente_id")
+    private Long idCliente;
     @Column(name = "nome")
     private String nome;
     @Column(name = "email")
