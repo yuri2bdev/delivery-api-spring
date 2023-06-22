f935cf2f5f5eb6227bcd051afca1eead61c2bea7
yuri2bdev <yuribritodev@gmail.com>
2023-06-22 00:33:29 -0300

    Update Entrega.java

--- src/main/java/br/com/delivery/deliveryapi/model/Entrega.java
+++ src/main/java/br/com/delivery/deliveryapi/model/Entrega.java
@@ -3,6 +3,7 @@ package br.com.delivery.deliveryapi.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
@@ -14,12 +15,12 @@ import javax.persistence.*;
 public class Entrega {
 
     @Id
-    @Column (name = "entrega_id")
-    private Long id;
-
+    @GeneratedValue(generator = "inc")
+    @GenericGenerator(name = "inc", strategy = "increment")
+    @Column(name = "entrega_id")
+    private Long idEntrega;
     @Column(name = "endereco")
     private String endereco;
-
     @ManyToOne
     @JoinColumn(name = "pedido_id")
     private Pedido pedido;
